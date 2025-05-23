964610375ca5a1c6d3752f5208e447799c10bd1a
João Marcos <joaofonsecan3@gmail.com>
2025-05-23 16:58:31 -0300

    Update identacao_blocos.py
    Retornei os valores anteriores ao meu teste.

--- 00 - Fundamentos/identacao_blocos.py
+++ 00 - Fundamentos/identacao_blocos.py
@@ -1,4 +1,13 @@
+def sacar(valor):
+    saldo = 1000
 
+
+    if saldo >= valor:
+        print("valor sacado!")
+        print("retire o seu dinheiro na boca do caixa.")
+
+    print("Obrigado por ser nosso cliente, tenha um bom dia!")
+sacar(100)
 
 
 def depositar(valor):
